0b2a630d9bab1ba3b11bd3a7181c4303e8b34540
divyanshusahu <dsahu1997@gmail.com>
2020-03-25 03:05:13 +0530

    frontend updated

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Web3 from "web3";
+import isEmpty from "is-empty";
 
 import {
   Pane,
@@ -10,6 +11,8 @@ import {
   Pill,
   TextInput,
   Button,
+  UnorderedList,
+  ListItem,
   toaster
 } from "evergreen-ui";
 
@@ -22,31 +25,72 @@ function TodoList() {
   const [newTaskInput, setNewTaskInput] = React.useState("");
   const [todoListTasks, setTodoListTasks] = React.useState([]);
 
-  let web3js;
+  let web3;
 
   const loadBlockchainData = async () => {
-    web3js = new Web3("http://localhost:8545");
-    const userAccount = await web3js.eth.getAccounts();
-    setAccount(userAccount[1]);
+    if (window.ethereum) {
+      // modern browser
+      if (process.env.NODE_ENV === "development") {
+        // Event subscriptions is not provided by HttpProvider.
+        //web3 = new Web3("http://localhost:8545"); // port 8545 ganache-cli port.
+        web3 = new Web3();
+        let eventProvider = new Web3.providers.WebsocketProvider(
+          "ws://localhost:8545"
+        );
+        web3.setProvider(eventProvider);
+      } else {
+        web3 = new Web3(window.ethereum);
+        try {
+          window.ethereum.enable();
+        } catch {
+          console.log("need to enable");
+        }
+      }
+    } else if (window.web3) {
+      // legacy browser
+      web3 = new Web3(window.web3.currentProvider);
+    } else {
+      // browser without Metamask.
+      toaster.danger("Error. Metamask not installed.", {
+        description: "You need to install Metamask for this app to work"
+      });
+    }
+
+    const userAccount = await web3.eth.getAccounts();
+    setAccount(userAccount[0]);
     const todoListContractAddress =
-      "0x9537b202e6B3b329e4a58373271BF46e63B11f49";
-    let tlc = new web3js.eth.Contract(todoListABI.abi, todoListContractAddress);
+      "0x23d78370Ef7732E85906980cA03A3F5Feee78feF"; // Paste your contract address here
+    let tlc = new web3.eth.Contract(todoListABI.abi, todoListContractAddress);
     setTodoListConnection(tlc);
+
     let tc = await tlc.methods.taskCount().call();
-    setTaskCount(tc);
+    setTaskCount(parseInt(tc));
     let t = [];
-    for (let i = 1; i <= taskCount; i++) {
+    for (let i = 1; i <= tc; i++) {
       let x = await tlc.methods.tasks(i).call();
       t.push(x);
     }
     setTodoListTasks(t);
 
-    tlc.events.taskCreated().on("data", event => {
-      let task = event.returnValues;
-      let temp = todoListTasks;
-      temp.push(task);
-      setTodoListTasks(temp);
-    });
+    /*
+      Subscriptions for an event in not supported by HttpProvider.
+      Need to look how to use WebSocket with ganache.
+    */
+    web3.eth
+      .subscribe(
+        "logs",
+        { fromBlock: "0x0", address: todoListContractAddress },
+        (error, result) => {
+          if (error) {
+            console.log(error);
+            return;
+          }
+          console.log(result.data);
+        }
+      )
+      .on("data", event => {
+        console.log(event);
+      });
   };
 
   React.useEffect(() => {
@@ -55,22 +99,32 @@ function TodoList() {
 
   const createTask = () => {
     if (newTaskInput.length < 10) {
+      console.log(todoListConnection.events.taskCreated());
       return toaster.danger("Task content too small", {
         description: "Task content must be ten characters long"
       });
     }
-    toaster.notify("Creating a new task");
+    toaster.notify("Creating a new task", { id: "createTaskToaster" });
+    /* 
+      In production mode we don't need to specifty the gas
+      amount. Metamask will take care of it automatically.
+      Either way the excess gas amount will be automatically
+      refunded to the user account.
+    */
     return todoListConnection.methods
       .createTask(newTaskInput)
-      .send({ from: account })
+      .send({ from: account, gas: 300000 })
       .on("receipt", receipt => {
-        console.log(receipt);
-        toaster.success("Task added successfully!");
-        setTaskCount(taskCount+1);
+        toaster.success("Task added successfully!", {
+          id: "createTaskToaster"
+        });
+        setTaskCount(taskCount + 1);
       })
       .on("error", error => {
-        console.log(error);
-        //toaster.danger("Task failed to add", { description: error });
+        toaster.danger("Task failed to add", {
+          description: error.message,
+          id: "createTaskToaster"
+        });
       });
   };
 
@@ -120,6 +174,21 @@ function TodoList() {
             <Button appearance="primary" iconBefore="add" onClick={createTask}>
               Create New Task
             </Button>
+          </div>
+          <div id="todolist_tasks" style={{ marginTop: "16px" }}>
+            <UnorderedList>
+              {isEmpty(todoListTasks)
+                ? null
+                : todoListTasks.map(t => (
+                    <ListItem
+                      key={t.id}
+                      icon={t.complete ? "tick-circle" : "ban-circle"}
+                      iconColor={t.complete ? "success" : "danger"}
+                    >
+                      {t.content}
+                    </ListItem>
+                  ))}
+            </UnorderedList>
           </div>
         </div>
       </Card>
