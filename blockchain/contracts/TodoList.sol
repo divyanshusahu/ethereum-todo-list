d91a28c51e9d718715041a3fb58bd432f1c40143
divyanshusahu <dsahu1997@gmail.com>
2020-03-26 02:07:27 +0530

    use of events removed.

--- blockchain/contracts/TodoList.sol
+++ blockchain/contracts/TodoList.sol
@@ -10,24 +10,26 @@ contract TodoList {
   }
 
   mapping(uint => Task) public tasks;
+  /*
+    event logging cost gas. Therefore it is removed.
 
-  event taskCreated(uint16 _id, string _content);
-  event taskCompleted(uint16 _id);
-  event taskContentEdited(uint16 _id);
-
+    event taskCreated(uint16 id, string content);
+    event taskStatusUpdated(uint16 id, bool complete);
+    event taskContentEdited(uint16 id, string content);
+  */
   function createTask(string calldata _content) external {
     taskCount++;
     tasks[taskCount] = Task(taskCount, _content, false);
-    emit taskCreated(taskCount, _content);
+    //emit taskCreated(taskCount, _content);
   }
 
-  function markTaskComplete(uint16 _id) external {
-    tasks[_id].complete = true;
-    emit taskCompleted(_id);
+  function updateTaskStatus(uint16 _id, bool _complete) external {
+    tasks[_id].complete = _complete;
+    //emit taskStatusUpdated(_id, _complete);
   }
 
   function editTaskContent(uint16 _id, string calldata _content) external {
     tasks[_id].content = _content;
-    emit taskContentEdited(_id);
+    //emit taskContentEdited(_id, _content);
   }
 }
